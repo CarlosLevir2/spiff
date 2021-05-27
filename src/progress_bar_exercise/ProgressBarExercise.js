29fd855cdaae4fda2d71c9c30da00164579aeb11
Carlos Levir <carloslevirfs@gmail.com>
2021-05-27 09:40:06 -0300

    feat: creates progress bar v1

--- src/progress_bar_exercise/ProgressBarExercise.js
+++ src/progress_bar_exercise/ProgressBarExercise.js
@@ -1,5 +1,7 @@
-import React from "react";
-import Exercise from "../exercise/Exercise";
+import React, { useState } from 'react';
+import Exercise from '../exercise/Exercise';
+
+import './styles.css';
 
 const ProgressBarExercise = () => {
   return (
@@ -17,6 +19,53 @@ export default ProgressBarExercise;
 
 // ----------------------------------------------------------------------------------
 
+function ProgressBar() {
+  const [buttonText, setButtonText] = useState('Start Request');
+  const [requestStatus, setRequestStatus] = useState('notFetched');
+  const [barVisibleStatus, setBarVisibleStatus] = useState('invisible');
+
+  function startRequest() {
+    if (requestStatus === 'fetching' || requestStatus === 'fetched') return;
+
+    setRequestStatus('fetching');
+    setBarVisibleStatus('visible');
+    setButtonText('Loading...');
+  }
+
+  function finishRequest() {
+    if (requestStatus !== 'fetching') return;
+
+    setRequestStatus('fetched');
+    setButtonText('Start Request');
+
+    setTimeout(() => {
+      setBarVisibleStatus('invisible');
+    }, 3000);
+
+    setTimeout(() => {
+      setRequestStatus('notFetched');
+    }, 4000);
+  }
+
+  return (
+    <div>
+      <div className={`progress-bar ${requestStatus} ${barVisibleStatus}`} />
+      <div>
+        <button className="button button-start" onClick={startRequest}>
+          {buttonText}
+        </button>
+        <button className="button button-finish" onClick={finishRequest}>
+          Finish Request
+        </button>
+      </div>
+    </div>
+  );
+}
+
 const Solution = () => {
-  return <div>Add solution here</div>;
+  return (
+    <div>
+      <ProgressBar />
+    </div>
+  );
 };
